102c11b3817044eeca02c1a7a9e4686c49ad3a28
kettek <kts@kettek.net>
2024-06-22 21:37:03 -0700

    Add crummy bot panel

--- internal/game/ui.go
+++ internal/game/ui.go
@@ -13,21 +13,34 @@ type UIOptions struct {
 
 type UI struct {
 	dudePanel DudePanel
+	roomPanel RoomPanel
 	options   *UIOptions
 }
 
 func NewUI() *UI {
 	ui := &UI{}
 
-	panelSprite := Must(render.NewSprite("ui/panels"))
-
-	ui.dudePanel = DudePanel{
-		top:      Must(render.NewSubSprite(panelSprite, 0, 0, 16, 16)),
-		topright: Must(render.NewSubSprite(panelSprite, 16, 0, 16, 16)),
-		mid:      Must(render.NewSubSprite(panelSprite, 0, 16, 16, 16)),
-		midright: Must(render.NewSubSprite(panelSprite, 16, 16, 16, 16)),
-		bot:      Must(render.NewSubSprite(panelSprite, 0, 32, 16, 16)),
-		botright: Must(render.NewSubSprite(panelSprite, 16, 32, 16, 16)),
+	{
+		panelSprite := Must(render.NewSprite("ui/panels"))
+		ui.dudePanel = DudePanel{
+			top:      Must(render.NewSubSprite(panelSprite, 0, 0, 16, 16)),
+			topright: Must(render.NewSubSprite(panelSprite, 16, 0, 16, 16)),
+			mid:      Must(render.NewSubSprite(panelSprite, 0, 16, 16, 16)),
+			midright: Must(render.NewSubSprite(panelSprite, 16, 16, 16, 16)),
+			bot:      Must(render.NewSubSprite(panelSprite, 0, 32, 16, 16)),
+			botright: Must(render.NewSubSprite(panelSprite, 16, 32, 16, 16)),
+		}
+	}
+	{
+		panelSprite := Must(render.NewSprite("ui/botPanel"))
+		ui.roomPanel = RoomPanel{
+			topleft:  Must(render.NewSubSprite(panelSprite, 0, 0, 16, 32)),
+			left:     Must(render.NewSubSprite(panelSprite, 0, 16, 16, 32)),
+			topmid:   Must(render.NewSubSprite(panelSprite, 16, 0, 16, 32)),
+			mid:      Must(render.NewSubSprite(panelSprite, 16, 16, 16, 32)),
+			topright: Must(render.NewSubSprite(panelSprite, 32, 0, 16, 32)),
+			right:    Must(render.NewSubSprite(panelSprite, 32, 16, 16, 32)),
+		}
 	}
 	return ui
 }
@@ -35,15 +48,20 @@ func NewUI() *UI {
 func (ui *UI) Layout(o *UIOptions) {
 	ui.options = o
 	ui.dudePanel.Layout(o)
+	ui.roomPanel.Layout(o)
 }
 
 func (ui *UI) Update(o *UIOptions) {
 	ui.dudePanel.Update(o)
+	ui.roomPanel.Update(o)
 }
 
 func (ui *UI) Draw(o *render.Options) {
 	o.DrawImageOptions.GeoM.Scale(ui.options.Scale, ui.options.Scale)
 	ui.dudePanel.Draw(o)
+	o.DrawImageOptions.GeoM.Reset()
+	o.DrawImageOptions.GeoM.Scale(ui.options.Scale, ui.options.Scale)
+	ui.roomPanel.Draw(o)
 }
 
 type DudePanel struct {
@@ -63,7 +81,7 @@ type DudePanel struct {
 func (dp *DudePanel) Layout(o *UIOptions) {
 	dp.height = o.Height - o.Height/3
 	// Position at vertical center.
-	dp.SetPosition(0, float64(o.Height/2)-float64(dp.height)/2)
+	dp.SetPosition(0, float64(o.Height/2)-float64(dp.height)/2-32)
 }
 
 func (dp *DudePanel) Update(o *UIOptions) {
@@ -115,4 +133,49 @@ func (dp *DudePanel) Draw(o *render.Options) {
 	dp.bot.Draw(o)
 	o.DrawImageOptions.GeoM.Translate(16, 0)
 	dp.botright.Draw(o)
+}
+
+type RoomPanel struct {
+	render.Originable
+	render.Positionable
+	width    int
+	left     *render.Sprite
+	topleft  *render.Sprite
+	mid      *render.Sprite
+	topmid   *render.Sprite
+	right    *render.Sprite
+	topright *render.Sprite
+}
+
+func (rp *RoomPanel) Layout(o *UIOptions) {
+	rp.width = o.Width - o.Width/3
+	rp.SetPosition(float64(o.Width/2)-float64(rp.width)/2, float64(o.Height)-96)
+}
+
+func (rp *RoomPanel) Update(o *UIOptions) {
+}
+
+func (rp *RoomPanel) Draw(o *render.Options) {
+	x := 0
+	o.DrawImageOptions.GeoM.Translate(rp.Position())
+	// topleft
+	rp.topleft.Draw(o)
+	o.DrawImageOptions.GeoM.Translate(0, 32)
+	// left
+	rp.left.Draw(o)
+	o.DrawImageOptions.GeoM.Translate(32, -32)
+	// mid
+	x += 16
+	for ; x < rp.width-16; x += 16 {
+		rp.topmid.Draw(o)
+		o.DrawImageOptions.GeoM.Translate(0, 32)
+		rp.mid.Draw(o)
+		o.DrawImageOptions.GeoM.Translate(0, -32)
+		o.DrawImageOptions.GeoM.Translate(16, 0)
+	}
+	// topright
+	rp.topright.Draw(o)
+	o.DrawImageOptions.GeoM.Translate(0, 32)
+	// right
+	rp.right.Draw(o)
 }
