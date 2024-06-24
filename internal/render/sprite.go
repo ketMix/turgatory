e77a7eee8ec0bce97914a5f3c3a492e9746ef220
kettek <kts@kettek.net>
2024-06-23 19:32:33 -0700

    Add sprite scale

--- internal/render/sprite.go
+++ internal/render/sprite.go
@@ -12,6 +12,11 @@ type Sprite struct {
 	Originable
 	Rotateable
 	image *ebiten.Image
+	Scale float64
+}
+
+func (s *Sprite) Size() (float64, float64) {
+	return float64(s.image.Bounds().Dx()) * s.Scale, float64(s.image.Bounds().Dy()) * s.Scale
 }
 
 func NewSprite(name string) (*Sprite, error) {
@@ -19,13 +24,17 @@ func NewSprite(name string) (*Sprite, error) {
 	if err != nil {
 		return nil, err
 	}
-	sprite := &Sprite{}
+	sprite := &Sprite{
+		Scale: 1,
+	}
 	sprite.image = dataSprite.Image
 	return sprite, nil
 }
 
 func NewSubSprite(dataSprite *Sprite, x, y, w, h int) (*Sprite, error) {
-	sprite := &Sprite{}
+	sprite := &Sprite{
+		Scale: 1,
+	}
 	sprite.image = dataSprite.image.SubImage(image.Rect(x, y, x+w, y+h)).(*ebiten.Image)
 	return sprite, nil
 }
@@ -40,6 +49,8 @@ func (s *Sprite) Draw(o *Options) {
 	opts.GeoM.Translate(-ox, -oy)
 	opts.GeoM.Rotate(s.Rotation())
 	opts.GeoM.Translate(ox, oy)
+
+	opts.GeoM.Scale(s.Scale, s.Scale)
 
 	opts.GeoM.Concat(o.DrawImageOptions.GeoM)
 
