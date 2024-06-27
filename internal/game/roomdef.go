0be0bc1de9fc4383125d327af898c38ea5bb7059
liqMix <liqmix@protonmail.com>
2024-06-26 23:38:13 -0600

    add room descriptions to room def

--- internal/game/roomdef.go
+++ internal/game/roomdef.go
@@ -50,3 +50,48 @@ func GetRoomDef(kind RoomKind, size RoomSize) *RoomDef {
 	RoomDefs[key] = r
 	return r
 }
+
+func (r *RoomDef) GetDescription() string {
+	switch r.kind {
+	case Stairs:
+		return "Stairs"
+	case Armory:
+		switch r.size {
+		case Medium:
+			return "Increases your equipment levels by 1"
+		case Large:
+			return "Increases your equipment levels by 5"
+		}
+	case HealingShrine:
+		switch r.size {
+		case Small:
+			return "Heals 25% of your health"
+		case Medium:
+			return "Heals 75% of your health"
+		case Large:
+			return "Heals 100% of your health"
+		}
+	case Combat:
+		return "Engage with enemies to gain gold and XP!"
+	case Well:
+		return "Restores your equipment uses"
+	case Treasure:
+		switch r.size {
+		case Small:
+			return "Contains a peasant's pittance"
+		case Medium:
+			return "Contains a squire's savings"
+		case Large:
+			return "Contains a baron's bounty"
+		case Huge:
+			return "Contains wealth beyond measure"
+		}
+	case Library:
+		return "A chance to enchant your equipment"
+	case Curse:
+		return "A chance to lose gold, equipment level, perk level, or dude level"
+	case Trap:
+		return "Watch your steppie!"
+	}
+	return "Unknown"
+}
